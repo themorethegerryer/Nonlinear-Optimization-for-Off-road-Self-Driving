d0cacc3d8cc90ca6348afe06515737baed0718b6
Gerry D'Ascoli <38154247+themorethegerryer@users.noreply.github.com>
2022-04-21 20:03:22 -0400

    Update DoubleTrackModel.py

--- src/DoubleTrackModel.py
+++ src/DoubleTrackModel.py
@@ -2,22 +2,23 @@ import numpy as np
 
 class DoubleTrackModel():
     #All vectors are in order of 0:FL, 1:FR, 2:RL, 3:RR
-    def __init__(self, ax=0.0, ay=0.0, r=0.0, Iw=0.0, w=[0.0,0.0,0.0,0.0], 
-                 Td=0.0, Tb=[0.0,0.0,0.0,0.0], Fx=[0.0,0.0,0.0,0.0], lw1=0.0,
-                 lw2=0.0, lr=0.0, lf=0.0, Rw=0.0, Fr=[0.0,0.0,0.0,0.0], 
+    def __init__(self, U0=[0.0,0.0], ax=0.0, ay=0.0, r=0.0, m=2000.0, w=[0.0,0.0,0.0,0.0], 
+                 Td=0.0, Tb=[0.0,0.0,0.0,0.0], lw1=1.8,
+                 lw2=1.8, lr=2.25, lf=2.25, Rw=0.23, Fr=[0.0,0.0,0.0,0.0], 
                  Vxwheel=[0.0,0.0,0.0,0.0], Vw=[0.0,0.0,0.0,0.0], 
-                 alpha=[0.0,0.0,0.0,0.0], beta=0.0, Vx=0.0, Vy=0.0,
-                 lamb=[0.0,0.0,0.0,0.0]):
+                 beta=0.0, Vx=0.0, Vy=0.0):
         
         self.parameters = {
             "ax"     : ax,
             "ay"     : ay,
             "r"      : r,
-            "Iw"     : Iw,
+            "m"      : m,
+            "Iw"     : 0.0,
+            "Iz"     : 0.0,
             "w"      : w, 
             "Td"     : Td,
             "Tb"     : Tb,
-            "Fx"     : Fx,
+            "Fx"     : [0.0,0.0,0.0,0.0],
             "lw1"    : lw1,
             "lw2"    : lw2,
             "lr"     : lr,
@@ -26,13 +27,18 @@ class DoubleTrackModel():
             "Fr"     : Fr,
             "Vxwheel": Vxwheel,
             "Vw"     : Vw,
-            "alpha"  : alpha,
+            "alpha"  : [0.0,0.0,0.0,0.0],
             "beta"   : beta,
             "Vx"     : Vx,
             "Vy"     : Vy,
-            "lambda" : lamb}
+            "lambda" : [0.0,0.0,0.0,0.0]}
         
-        self.update_parameters([0.0,0.0,0.0])
+        self.state = np.zeros(13)
+        self.state[3] = 1 #Initialize unit quaternion
+        self.state[7] = self.parameters["Vx"]
+        self.state[8] = self.parameters["Vy"]
+        
+        self.update_parameters(U0)
         
     def update_parameters(self, U):
         theta_left, theta_right = self.ackermann_turn(U[0])
@@ -47,6 +53,10 @@ class DoubleTrackModel():
                 self.parameters["lambda"][i] = lambda_check/self.parameters["Vxwheel"][i]
             else:
                 self.parameters["lambda"][i] = lambda_check/(self.parameters["w"][i]*self.parameters["Rw"])
+                
+        #Iw
+        #Iz
+        #Fx 
         
         
         
