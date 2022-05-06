674aa2bdd26fa05f579f8b37e0785ea566a7e491
Feng Xiang <fengxiang@MacBook-Pro-2.home>
2022-05-06 00:26:37 -0400

    added tire and Fz equations.

--- src-matlab/DoubleTrackModel.m
+++ src-matlab/DoubleTrackModel.m
@@ -33,31 +33,39 @@ classdef DoubleTrackModel
     methods
         function car = DoubleTrackModel()
             % Double Track Model Constructor
+            car.L = car.a + car.b;
         end
         
         function dxdt = continuous_dynamics(car, x, u)% ax, ay, rdot, wdot
             % x = [uy r ux dPsi e dFzlong dFzlat delta]
             % u = [deltadot Fxflbrake Fxfrbrake Fxrl Fxrr Fengine udiff]
-            
+            % TODO equation of motion go here
         end
         
-        function X_n_1 = dynamics_rk4(car,X,U,timeVal,dt)
-            uTemp = U;
+        function x_next = dynamics_rk4(car,x,u,dt)
+            uTemp = u;
 %             uTemp(1) = uTemp(1) + 0.1*timeVal;
 %             if mod(timeVal,3) == 0
 %                uTemp(1) = -1 * uTemp(1); 
 %             end
-            f1 = continuous_dynamics(car, X, uTemp);
-            f2 = continuous_dynamics(car, X + 0.5*dt*f1, uTemp);
-            f3 = continuous_dynamics(car, X + 0.5*dt*f2, uTemp);
-            f4 = continuous_dynamics(car, X + dt*f3, uTemp);
+            f1 = continuous_dynamics(car, x, uTemp);
+            f2 = continuous_dynamics(car, x + 0.5*dt*f1, uTemp);
+            f3 = continuous_dynamics(car, x + 0.5*dt*f2, uTemp);
+            f4 = continuous_dynamics(car, x + dt*f3, uTemp);
             
-            X_n_1 = X + (dt / 6)*(f1 + 2*f2 + 2*f3 + f4);
-            X_n_1(isnan(X_n_1)) = 0;
+            x_next = x + (dt / 6)*(f1 + 2*f2 + 2*f3 + f4);
+            x_next(isnan(x_next)) = 0;
         end
         
         function jac = discrete_jacobian(car, x, u)
+            % TODO fill out
+        end
 
+        function Fz_bundle = tire_fz_function(car, x, Fz)
+            Fz_bundle.fl = (((car.b/car.L)*Fz - x(6))/2) - car.gamma*u(7); % eq 4.13
+            Fz_bundle.fr = (((car.b/car.L)*Fz - x(6))/2) + car.gamma*u(7); % eq 4.14
+            Fz_bundle.rl = (((car.a/car.L)*Fz + x(6))/2) - (1 - car.gamma)*u(7); % eq 4.15
+            Fz_bundle.rr = (((car.a/car.L)*Fz + x(6))/2) + (1 - car.gamma)*u(7); % eq 4.16
         end
 
         function tire_corner_stiffness = cornering_stiffnes(car, tire_mu, Fz, Fx) % eq 4.20
@@ -75,30 +83,45 @@ classdef DoubleTrackModel
         end
 
         
-        function Fwheel = tire_model(car, longSlip, sideSlip, Fz, Fx) % TODO replace Fx with the state of the car
-            % compute wheel maximum lateral force Fy
+        function Fwheel = tire_model(car, longSlip, sideSlip, Fz, Fx, u, front_bool) 
+            % TODO replace Fx with the state of the car
+            % computer lateral force Fy
             tire_mu = tire_friction(car,Fz);
+            c_stiffness = cornering_stiffness(car, tire_mu, Fz, Fx);
             Fy_max = sqrt((tire_mu*Fz)^2 - Fx); % eq. 4.17
             
-            alpha_slide = (3*Fy_max) / cornering_stiffness(car, tire_mu, Fz, Fx); % eq 4.18
-            
+            alpha_slide = (3*Fy_max) / c_stiffness; % eq 4.18
+
+
+            Fwheel.Fy = -c_stiffness*tan(sideSlip) + (c_stiffness^2 / (3*Fy_max))*tan(sideSlip)*abs(tan(sideSlip));
+            if abs(sideSlip) <= alpha_slide
+                Fwheel.Fy = Fwheel.Fy - (c_stiffness^3 / (27*Fy_max^2))*tan(sideSlip)^3;
+            else
+                Fwheel.Fy = Fwheel.Fy - Fy_max*sign(sideSlip);
+            end
+
+            % compute longitudinal force Fx
+            % assume constant open differential
+            torque_engine = u(6);
+            if front_bool == true
+                Fwheel.Fx_left = torque_engine/2 - u(2);
+                Fwheel.Fx_right = torque_engine/2 - u(3);
+            else
+                Fwheel.Fx_left = 0;
+                Fwheel.Fx_right = 0;
+            end
 
         end
         
-        function slip = slip_function(car, x,u)
-            % x = [uy r ux dPsi e dFzlong dFzlat delta]
-            % u = [deltadot Fxflbrake Fxfrbrake Fxrl Fxrr Fengine udiff]
-        end
-        
         function theta = ackermann_turn(car, steering_angle)
-            car_length = car.a + car.b;
-            radius = car_length*tan((pi/2)-steering_angle);
+            % TODO may not need to use for this model
+            radius = car.L*tan((pi/2)-steering_angle);
             if steering_angle < 0
-                theta.left = atan(car_length/(radius+(car.df/2))); % *(180/pi);
-                theta.right = atan(car_length/(radius-(car.df/2))); % *(180/pi);
+                theta.left = atan(car.L/(radius+(car.df/2))); % *(180/pi);
+                theta.right = atan(car.L/(radius-(car.df/2))); % *(180/pi);
             else
-                theta.right = atan(car_length/(radius+(car.df/2))); % *(180/pi);
-                theta.left = atan(car_length/(radius-(car.df/2))); % *(180/pi);
+                theta.right = atan(car.L/(radius+(car.df/2))); % *(180/pi);
+                theta.left = atan(car.L/(radius-(car.df/2))); % *(180/pi);
             end
         end
     end
