caa0d32e67c62d1c1e1c638e7bd5a643f2231e89
Gerry D'Ascoli <38154247+themorethegerryer@users.noreply.github.com>
2022-05-11 02:18:50 -0400

    Cleaning up commit

--- src-matlab/iLQR_test.m
+++ src-matlab/iLQR_test.m
@@ -7,16 +7,20 @@ U = zeros(2,N-1);
 xg = XrefDouble(7:9);
 % XDouble = [uy r ux dFzlong dFzlat delta xPos yPos yawOrient]
 x = XDouble;
-for k = 1:50
+for k = 1:60
     [u0,U,X] = MPC_iLQR(x, XrefDouble, U);
 %     U = U(:,2:end);
     x = [u0(2)*cos(X(3,2)) 0 u0(2)*sin(X(3,2)) 0 0 u0(1) X(1,2) X(2,2) X(3,2)];
     figure(1)
     clf(1)
     hold on
-    plot(X(1,1),X(2,2),'b-o')
     plot(X(1,:),X(2,:),'y-o')
     plot(xg(1),xg(2),'r-o')
-    axis([-10 150 -10 150])
-    pause(0.1)
+    plot(X(1,1),X(2,1),'b-o')
+    axis([-5 105 -5 105])
+    xlabel("x")
+    ylabel("y")
+    t = (k-1)*0.1;
+    title("t = "+t)
+    pause;%(0.1)
 end
